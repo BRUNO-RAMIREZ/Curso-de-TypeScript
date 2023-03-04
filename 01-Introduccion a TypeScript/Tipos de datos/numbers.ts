3b9e4ea78b7c3a955535f008aef130c1ab86d5c4
BRUNO-RAMIREZ <brunaisdiaz1@gmail.com>
2023-03-04 17:54:38 -0400

    Tipos de datos - Numbers

---
+++ 01-Introduccion a TypeScript/Tipos de datos/numbers.ts
@@ -0,0 +1,6 @@
+(() => {
+    let typeNumber: number = 10;
+    const typeConstNumber: number = 20;
+    console.log({typeNumber, typeConstNumber})
+    //Not a number = NaN = Es de tipo number
+})()
